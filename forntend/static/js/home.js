2267958fc4b839e0220aed5359ab52d8d727e7a5
Taron Hakobyan <taronhakobyan026@gmail.com>
2024-08-30 13:56:43 -0700

    Added home page

---
+++ forntend/static/js/home.js
@@ -0,0 +1,14 @@
+const searchInput = document.getElementById('search-input');
+const clubItems = document.querySelectorAll('.club-item');
+
+searchInput.addEventListener('input', function() {
+    const searchQuery = searchInput.value.toLowerCase();
+    clubItems.forEach(item => {
+        const clubName = item.querySelector('p').textContent.toLowerCase();
+        if (clubName.includes(searchQuery)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
