c3a3714d33d5dd218ef1f4c4f1458c37841722d0
Artak Botoian <botoian@Artaks-MBP.lan>
2024-09-01 16:10:06 -0700

    clublist is finished, need to add footer

--- frontend/react/src/components/ClubsPictures.js
+++ frontend/react/src/components/ClubsPictures.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+import data from "../data/data";
+import "./ClubsPictures.css";
 
 function ClubsPictures() {
   return (
@@ -13,7 +16,21 @@ function ClubsPictures() {
         <div>Hobby</div>
         <div>Sports</div>
       </div>
-      <div className="club-pictures"></div>
+      <div className="clubs-pictures">
+        {data.map((item, index) => {
+          return (
+            <Card
+              border="dark"
+              style={{ width: window.innerWidth <= 768 ? "11rem" : "12rem" }}
+            >
+              <Card.Img variant="top" src={item.imageUrl} height="200px" />
+              <Card.Body>
+                <Card.Title className="clubs-title">{item.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 }
