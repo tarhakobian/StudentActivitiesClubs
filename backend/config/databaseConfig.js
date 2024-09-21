ddc8665d953e4a1896311dedf339fd1c3b57ab49
Taron Hakobyan <taronhakobyan026@gmail.com>
2024-09-21 02:15:50 -0700

    Minor change in databaseConfig.js

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URL)
     .then(r => console.log("Connected to the database"))
-    .catch(reason => console.log(`Database connection Failed --- ${reason}`))
+    .catch(reason => {
+        console.log(`Database connection Failed --- ${reason}`)
+        process.exit(0);
+    })
 
 module.exports = mongoose
 
