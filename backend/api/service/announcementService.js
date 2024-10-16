a538e9d0450cf11877796f338701254d8087d90a
Taron Hakobyan <taronhakobyan026@gmail.com>
2024-10-16 02:29:57 -0700

    test

--- backend/api/service/announcementService.js
+++ backend/api/service/announcementService.js
@@ -8,7 +8,6 @@ const { mapAnnuncement } = require('./mapperService');
 const Association = require('../../database/model/associationModel');
 
 
-
 async function getAllAnnouncements(clubId, userId) {
     await ensureOwnership(clubId, userId)
 
